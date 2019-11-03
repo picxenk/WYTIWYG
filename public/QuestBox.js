3eb5228f19f61eaa7e4408960ad8687a20053498
Seungbum Kim <picxenk@gmail.com>
2019-11-04 01:22:26 +0900

    add some quests

--- public/QuestBox.js
+++ public/QuestBox.js
@@ -13,23 +13,39 @@ class QuestBox {
             // {type: 'c', ko: "" , en: ""},
             // {type: 'p', ko: "" , en: ""},
             {type: 'm', ko: "1 더하기 1 은 1 ?" , en: "1 plus 1 is 1?"},
-            {type: 'c', ko: "굴을 즐겨 먹는가?" , en: "Do you enjoy eating oysters?"},
+            {type: 'c', ko: "지구는 평평한가?" , en: "Is the earth flat?"},
             {type: 'p', ko: "세상은 평등한가?" , en: "Is the world equal?"},
+            {type: 'm', ko: "9는 짝수인가?" , en: "Is 9 even?"},
+            {type: 'c', ko: "자원은 무한한가?" , en: "Are resources unlimited?"},
+            {type: 'p', ko: "좌파는 항상 옳은가?" , en: "Is the left always right?"},
+            {type: 'm', ko: "8은 4보다 작은가?" , en: "Is 8 less than 4?"},
+            {type: 'c', ko: "당신은 항상 옳은가?" , en: "Are you always right?"},
+            {type: 'p', ko: "우파는 항상 옳은가?" , en: "Is the right always right?"},
         ];
         this.quests1 = [
             {type: 'm', ko: "1 더하기 1 은 2 ?" , en: "1 plus 1 is 2?"},
             {type: 'c', ko: "고양이를 좋아하는가?" , en: "Do you like a cat?"},
             {type: 'p', ko: "정의는 실현가능한가?" , en: "Is justice possible?"},
+            {type: 'm', ko: "7은 홀수인가?" , en: "Is 9 odd?"},
+            {type: 'c', ko: "김치를 아는가?" , en: "Do you know kimchi?"},
+            {type: 'c', ko: "인간은 달에 착륙했는가?" , en: "Did humans land on the moon?"},
         ];
         this.quests01 = [
             {type: 'm', ko: "4917은 소수이다?" , en: "Is 4917 a prime number?"},
             {type: 'c', ko: "벤쿠버는 캐나다의 수도이다?" , en: "Is Vancouver the capital of Canada?"},
             {type: 'p', ko: "구글이 우리를 지배하는가?" , en: "Does Google rule us?"},
+            {type: 'c', ko: "굴을 즐겨 먹는가?" , en: "Do you enjoy eating oysters?"},
+            {type: 'c', ko: "빛의 3원색은 빨강 파랑 노랑인가?" , en: "The three primary colors of light are red, blue, yellow?"},
+            {type: 'p', ko: "짜장보다는 짬뽕?" , en: "Champon rather than jjajang?"},
+            {type: 'c', ko: "지구의 중력은 일정한가?" , en: "Is the Earth's gravity constant?"},
         ];
         this.quests10 = [
             {type: 'm', ko: "4919는 소수이다?" , en: "Is 4919 a prime number?"},
             {type: 'c', ko: "캔버라는 오스트레일리아의 수도이다?" , en: "Is Canberra the capital of Australia?"},
             {type: 'p', ko: "아이폰11을 살것인가?" , en: "Will you buy an iPhone 11?"},
+            {type: 'p', ko: "정의는 실현가능한가?" , en: "Is justice possible?"},
+            {type: 'c', ko: "고양이를 좋아하는가?" , en: "Do you like a cat?"},
+            {type: 'c', ko: "개를 좋아하는가?" , en: "Do you like a dog?"},
         ];
     }
 
